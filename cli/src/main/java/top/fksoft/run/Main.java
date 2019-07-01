b159617aa9b554c18a3a7bae4bf6d80763961be6
Noob <ExplodingFKL@gmail.com>
2019-07-01 09:54:45 +0800

    first Commit

---
+++ cli/src/main/java/top/fksoft/run/Main.java
@@ -0,0 +1,10 @@
+package top.fksoft.run;
+
+import top.fksoft.httpServer.HttpServer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("HelloWorld");
+        new HttpServer();
+    }
+}
---
+++ core/src/main/java/top/fksoft/httpServer/HttpServer.java
@@ -0,0 +1,7 @@
+package top.fksoft.httpServer;
+
+public class HttpServer {
+    public HttpServer() {
+        System.err.println("Run");
+    }
+}
