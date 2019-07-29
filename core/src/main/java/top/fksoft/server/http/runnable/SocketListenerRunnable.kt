4b0ab59420aa02d2a701d547008e7e1b060945f9
Noob <ExplodingFKL@gmail.com>
2019-07-29 23:53:55 +0800

    fix bugs.

--- core/src/main/java/top/fksoft/server/http/runnable/SocketListenerRunnable.kt
+++ core/src/main/java/top/fksoft/server/http/runnable/SocketListenerRunnable.kt
@@ -74,6 +74,7 @@ constructor(private val httpServer: HttpServer, private val serverSocket: Server
             } catch (e: Exception) {
                 logger.error("在处理 $remoteInfo 的过程中出现异常.", e)
             }
+
         }
     }
 
